81821c99082ceb73e9574f71f6b0add328d19199
Minh Pham <minhpham@myhost.local>
2017-12-29 14:17:08 +0700

    2 - Finalize weather app

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-## 2 Your First App
+## 2 - Your First App
 
 [https://academy.plot.ly/react/2-first-app/](https://academy.plot.ly/react/2-first-app/)
+
+### Note:
+
+1. - App.js: °  = Option + Shift + 8
+2. - App.js: xhr for request API
+
+```
+	xhr({
+      url: url
+    }, function(err, data){
+      // save data here
+      self.setState({
+        data: JSON.parse(data.body)
+      })
+    })
+```
+
+3. - App.js: var self = this; ( inside the function of function )
+
+4. - App.js: encodeURIComponent('My string with spaces'); // -> 'My%20string%20with%20spaces'
+
+5. - App.js: network request data is a string, so we’ll need to parse that with JSON.parse to make it an object.
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import xhr from 'xhr';
+
 class App extends Component {
+  state = {
+    location: '',
+    data: {}
+  }
+
+  fetchData = (event) => {
+    event.preventDefault();
+  
+    var location = encodeURIComponent(this.state.location);
+
+    var urlPrefix = "http://api.openweathermap.org/data/2.5/forecast?q=";
+    var urlSuffix = "&APPID=c1f62f052159942bc0c53780caf947bd&units=metric";
+    var url = urlPrefix + location + urlSuffix;
+    
+    var self = this;
+    xhr({
+      url: url
+    }, function(err, data){
+      // save data here
+      self.setState({
+        data: JSON.parse(data.body)
+      })
+    })
+  }
+
+  changeLocation = (event) => {
+    console.log('changeLocation')
+    this.setState({
+      location: event.target.value
+    })
+  }
+
   render() {
+    var currentTemp = 'not loaded yet';
+    
+    if (this.state.data.list){
+      currentTemp = this.state.data.list[0].main.temp;
+    }
+
     return (
       <div>
-        Weather
+        <h1>Weather</h1>
+        <form onSubmit={this.fetchData}>
+          <label>I want to know the weather for
+            <input 
+              type="text" 
+              placeholder={"City, Country"}
+              value={this.state.location}
+              onChange={this.changeLocation}
+            />
+          </label>
+        </form>
+        <p>
+          <span>{ currentTemp }</span>
+          <span> °C</span>
+        </p>
       </div>
     );
   }
